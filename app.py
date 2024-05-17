be4b49e8e0495d8176d5ea21491014e1c67f7807
hanju.kim <wptlzk6404@naver.com>
2024-05-17 16:46:50 +0900

    csv 비동기 호출 추가

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ from datetime import datetime
 import requests
 import json
 import pandas as pd
-
+import csv
+import asyncio
 import streamlit as st
 from streamlit_chat import message
 
 base_url = "https://live-stargate.sionic.im/api"
 api_key = st.secrets["api_key"]
 
+
 #st.header("Chatbot LMM 🤖")
 st.header("Chatbot LMM PoC")
 
+q_list = []
+r_list = []
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
@@ -21,7 +25,7 @@ if 'past' not in st.session_state:
     st.session_state['past'] = []
  
 # 파일 업로드 함수
-# 디렉토리 이름, 파일 imput
+# 디렉토리 이름, 파일 input
 def save_uploaded_file(directory, file):
     # 1. 저장할 디렉토리 있는지 확인
     if not os.path.exists(directory):
@@ -36,6 +40,13 @@ def save_uploaded_file(directory, file):
 def postRequest(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
+
+async def process(thread_list, question_list):
+    tasks = []
+    for i in range(0, len(thread_list)):
+        tasks.append(asyncio.create_task(send_chat(thread_list[i], question_list[i])))
+
+    await asyncio.wait(tasks)
 
 # 채팅 쓰레드 생성
 def create_thread(bucket_id):
@@ -56,8 +67,8 @@ def create_thread(bucket_id):
 
     return thread_id
 
-#
-def send_chat(thread_id, question):
+# 채팅 전송
+async def send_chat(thread_id, question):
     url = base_url + f"/v1/threads/{thread_id}/chats"
     payload = {
         "question": question,
@@ -68,9 +79,7 @@ def send_chat(thread_id, question):
         "content-type": "application/json",
         "x-storm-token": api_key
     }
-
     response = requests.post(url, json=payload, headers=headers, stream=True)
-
     text = ""
 
     for chunk in response.iter_lines(decode_unicode=True):
@@ -81,7 +90,7 @@ def send_chat(thread_id, question):
                 is_final_event = data.get("is_final_event")
 
                 if content:
-                    print(content, end="", flush=True)
+                    #print(content, end="", flush=True)
                     text = text + content
 
                 if is_final_event:
@@ -89,8 +98,9 @@ def send_chat(thread_id, question):
             except json.JSONDecodeError as e:
                 print(f"Error decoding JSON: {e}")
                 continue
+    q_list.append(question)
+    r_list.append(text)
     return text
- 
 
 menu = ['직접 입력', '엑셀 업로드']
 choice = st.sidebar.selectbox('메뉴', menu)
@@ -106,18 +116,10 @@ if choice == menu[0]:
     if submitted and user_input:
         bucket_id = "7194191884929994753"
         thread_id = create_thread(bucket_id)
-        #chaturl = "https://ibot.kt.com/client/v1/talk"
-        #chatheader = {"Content-Type":"application/json"}
-        #chatpayload = {"sessionKey":"gw32_859b7f89da7f4cf3a2a38fcd9d5a317e","channelToken":"777e7a05a5654253976483a94d20454c","query":user_input,"voice":"false","type":"TALK","id":"hjlv0rN9H3jCGkywO6DU","currentTime":"오전 10:47","className":"name","personalData":"false","displayText":"아이케어"}
-        #chatres = requests.post(chaturl, headers=chatheader, json=chatpayload)
-
-        #print(chatres.json()["data"]["messages"][0]["message"])
-        #st.session_state.generated.append(chatres.json()["data"]["messages"][0]["message"])
 
         st.session_state.past.append(user_input)
-
         st.session_state.generated.append(send_chat(thread_id, user_input))
-        
+
         #st.session_state.past.append(user_input2)
         #st.session_state.generated.append(user_input2+" Answer")
 
@@ -131,14 +133,61 @@ elif choice == menu[1]:
 
     csv_file = st.file_uploader('CSV 파일 업로드', type=['csv'])
     print(csv_file)
-    if csv_file is not None:
-        current_time = datetime.now()
-        filename = current_time.isoformat().replace(':', '_') + '.csv'
 
-        csv_file.name = filename
+    if st.button('SEND'):
+        if csv_file is not None:
+            current_time = datetime.now()
+            filename = current_time.isoformat().replace(':', '_') + '.csv'
 
-        save_uploaded_file('csv', csv_file)
+            csv_file.name = filename
 
-        # csv를 보여주기 위해 pandas 데이터 프레임으로 만들어야한다.
-        df = pd.read_csv('csv/'+filename)
-        st.dataframe(df)
+            save_uploaded_file('csv', csv_file)
+
+            df = pd.read_csv('csv/'+filename)
+            rf = open('csv/'+filename,'rt', encoding='UTF8')
+            reader = csv.reader(rf)
+
+            cnt = 0;
+            task_thread_list = [];
+            task_question_list = [];
+
+            for line in reader:
+                if cnt > 0:
+                    print(line[0])
+
+                    if line:
+                        bucket_id = "7194191884929994753"
+                        thread_id = create_thread(bucket_id)
+                        task_thread_list.append(thread_id)
+                        task_question_list.append(line[0])
+                        
+                cnt = cnt + 1
+
+            asyncio.run(process(task_thread_list, task_question_list))
+                
+            rf.close()
+            os.remove('csv/'+filename)
+
+            df = pd.DataFrame()
+            df['Question'] = q_list
+            df['Answer'] = r_list
+            st.dataframe(df)
+            r_list = []
+
+
+            def convert_df(df):
+                return df.to_csv(index=False).encode('utf-8')
+
+            csv = convert_df(df)
+
+            st.download_button(
+                "Press to Download",
+                csv,
+                "result.csv",
+                "text/csv",
+                key='download-csv'
+            )
+
+        else: st.warning('CSV 파일을 업로드 해주세요', icon="⚠️")
+        
+        
